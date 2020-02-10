15bdd937798e93de5de66dc8331b067aa8d019a0
Surajprakash Ankush <surajankush@gmail.com>
2020-02-11 00:02:21 +0530

    git actions test

---
+++ src/test/java/com/github/ActionDemo/ActionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ActionDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
